c382f5202b1a942fb02082df4672b1b3f28280bc
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-05-25 12:05:07 +0200

    typo/formatting

--- js/auction-functions.js
+++ js/auction-functions.js
@@ -1,14 +1,14 @@
 //AUCTION LISTING
 const games = [{name: "Avorion"}, {name: "PrisonArchietects"}, {name: "Skyrim"}, {name: "Elden Ring"}, {name: "Fallot"}];
 const users = [{user: "Moonfly"}, {user: "MRcool"}, {user: "Zhico"}, {user: "Grimshot"}, {user: "Rexxar"}];
-let auctionArray = "";
 
 function createList() {
 
     for(let i = 0; i < users.length; i++) {
 
-        auctionSeller = users.user[i]
-    }console.log(auctionSeller);
+        let auctionSeller = users.user[i];
+        console.log(auctionSeller);
+    }
 }
 
 createList();
