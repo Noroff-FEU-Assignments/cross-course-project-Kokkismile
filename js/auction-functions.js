d071c011310159cc8219a6915752206568e1963d
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-05-26 16:00:08 +0200

    creating auction function

--- js/auction-functions.js
+++ js/auction-functions.js
@@ -1,31 +1,31 @@
 //AUCTION LISTING
 //Example users and games
-const games = [{name: "Avorion"}, {name: "PrisonArchietects"}, {name: "Skyrim"}, {name: "Elden Ring"}, {name: "Fallout"}];
-const users = [{user: "Moonfly"}, {user: "MRcool"}, {user: "Zhico"}, {user: "Grimshot"}, {user: "Rexxar"}];
+const games = [{name: "Avorion", user: "Moonfly"}, {name: "PrisonArchietects", user: "MRcool"}, {name: "Skyrim", user: "Zhico"}, {name: "Elden Ring", user: "Grimshot"}, {name: "Fallout", user: "Rexxar"}];
 
 //Listing container
 const listContainer = document.querySelector(".auction-listing-container");
-const auctionListing = listContainer.innerHTML = `<div class="auction-listing-wrap">
+/* let auctionListing = listContainer.innerHTML = `<div class="auction-listing-wrap">
                                                     <p>test</p>
                                                     <p>test</p>
                                                     <p>test</p>
                                                     <p>test</p>
-                                                  </div>`;
+                                                  </div>`; */
 
 //Creating auction list
 function createList() {
 
-    for(let i = 0; i < users.length; i++) {
-
-        let auctionSeller = users[i].user;
-        console.log(auctionSeller);
-    }
 
     for(let i = 0; i < games.length; i++) {
 
         let auctionGame = games[i].name;
-        console.log(auctionGame);
+        let auctionSeller = games[i].user
+
+        listContainer.innerHTML += `<div class="auction-listing-wrap">
+        <p>${auctionGame}</p>
+        <p>${auctionSeller}</p>
+      </div>`;
     }
+
 }
 
 createList();
