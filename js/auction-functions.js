1ffae36ff64447b952af3b74e4f460eb5ca272b9
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-05-26 12:39:17 +0200

    creating auction function

--- js/auction-functions.js
+++ js/auction-functions.js
@@ -2,12 +2,20 @@
 const games = [{name: "Avorion"}, {name: "PrisonArchietects"}, {name: "Skyrim"}, {name: "Elden Ring"}, {name: "Fallout"}];
 const users = [{user: "Moonfly"}, {user: "MRcool"}, {user: "Zhico"}, {user: "Grimshot"}, {user: "Rexxar"}];
 
+const listContainer = document.querySelector(".auction-listing-container");
+
 function createList() {
 
     for(let i = 0; i < users.length; i++) {
 
         let auctionSeller = users[i].user;
         console.log(auctionSeller);
+    }
+
+    for(let i = 0; i < games.length; i++) {
+
+        let auctionGame = games[i].name;
+        console.log(auctionGame);
     }
 }
 
