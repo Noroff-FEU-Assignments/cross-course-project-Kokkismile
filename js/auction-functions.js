2cc223b0af7824b95f8ff38a6d9329fb78303a2c
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-05-26 13:23:17 +0200

    creating and styling auction function

--- js/auction-functions.js
+++ js/auction-functions.js
@@ -1,10 +1,18 @@
 //AUCTION LISTING
+//Example users and games
 const games = [{name: "Avorion"}, {name: "PrisonArchietects"}, {name: "Skyrim"}, {name: "Elden Ring"}, {name: "Fallout"}];
 const users = [{user: "Moonfly"}, {user: "MRcool"}, {user: "Zhico"}, {user: "Grimshot"}, {user: "Rexxar"}];
 
+//Listing container
 const listContainer = document.querySelector(".auction-listing-container");
-const auctionListing = listContainer.innerHTML = `<p>test</p>`;
+const auctionListing = listContainer.innerHTML = `<div class="auction-listing-wrap">
+                                                    <p>test</p>
+                                                    <p>test</p>
+                                                    <p>test</p>
+                                                    <p>test</p>
+                                                  </div>`;
 
+//Creating auction list
 function createList() {
 
     for(let i = 0; i < users.length; i++) {
