3d5a2100fdcfdb1dfa7803a898027c4a822bf182
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-06-01 19:27:58 +0200

    creating cart

---
+++ js/cart.js
@@ -0,0 +1,21 @@
+let cartContainer = document.querySelector(".cart-items-container");
+
+let cartStorage = JSON.parse(localStorage.getItem("cartArray"));
+console.log(cartStorage);
+
+cartContainer.innerHTML = `<p>Cart is empty</p>`;
+
+function asembleCart() {
+    cartContainer.innerHTML = ``;
+
+    for(i = 0; i < cartStorage.length; i++) {
+        cartContainer.innerHTML = `<div class="cart-items-bg">
+                                     <div class="cart-item-img" style="background-image: url(${cartStorage[0]});"></div>
+                                     <p>${cartStorage[2]}</p>
+                                     <p>Game ID: ${cartStorage[1]}</p>
+                                     <p class="text-green">Price: ${cartStorage[4]}</p>
+                                   </div>`;
+    }
+}
+
+asembleCart();
