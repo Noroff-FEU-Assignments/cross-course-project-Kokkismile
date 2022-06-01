4d56400fab950c6599ca9fb4e07a7d911e1b4113
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-06-01 19:46:32 +0200

    div

--- js/cart.js
+++ js/cart.js
@@ -11,9 +11,11 @@ function asembleCart() {
     for(i = 0; i < cartStorage.length; i++) {
         cartContainer.innerHTML = `<div class="cart-items-bg">
                                      <div class="cart-item-img" style="background-image: url(${cartStorage[0]});"></div>
-                                     <p>${cartStorage[2]}</p>
-                                     <p>Game ID: ${cartStorage[1]}</p>
-                                     <p class="text-green">Price: ${cartStorage[4]}</p>
+                                     <div class="cart-item-info">
+                                       <p>${cartStorage[2]}</p>
+                                       <p>Game ID: ${cartStorage[1]}</p>
+                                       <p class="text-green">Price: ${cartStorage[4]}</p>
+                                     </div>
                                    </div>`;
     }
 }
