a656919ad8fca565d985a5cd44328f8d36de6aa8
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-06-05 19:32:56 +0200

    formatting

--- js/underline-active.js
+++ js/underline-active.js
@@ -1,5 +1,7 @@
 const activePage = window.location;
+
 const navLinks = document.querySelectorAll("nav a").forEach(link => {
+    
     if(link.href.includes(`${activePage}`)) {
         link.classList.add("active");
     }
