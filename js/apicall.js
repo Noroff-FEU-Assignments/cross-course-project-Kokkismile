bcbe9f8064aac429bddecc6baf00a6718be6dbdc
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2022-08-22 14:13:14 +0200

    recreating apicall to do wordpress instead of rawg

--- js/apicall.js
+++ js/apicall.js
@@ -1,5 +1,5 @@
-const apiKey = "7e4f8935456c47bbb745861dedea99ef";
-const gameUrl = "https://api.rawg.io/api/games?key=";
+//const apiKey = "7e4f8935456c47bbb745861dedea99ef";
+const productsUrl = "http://localhost/gamehub-wp-api/wordpress-6.0.1/wordpress/wp-json/wc/store/products";
 
 const gameCard = document.querySelector(".game-cards");
 gameCard.innerHTML = `<h1>LOADING ⌛️</h1>`;
@@ -9,21 +9,22 @@ async function fetchGames() {
 
     try {
 
-        const response = await fetch(gameUrl + apiKey);
+        const response = await fetch(productsUrl);
         const json = await response.json();
+        console.log(json)
 
         gameCard.innerHTML = "";
         
-        const results = json.results;
+        const results = json.products;
 
-        for(i = 0; i < results.length; i++) {
+        for(i = 0; i < json.length; i++) {
 
             gameCard.innerHTML += `<div class="store-card-wrap">
                                        <div class="card-styling">
-                                         <a href="store-details.html?id=${results[i].id}" class="card">
-                                         <h2 class="padding-10px">${results[i].name}</h2>
+                                         <a href="store-details.html?id=0" class="card">
+                                         <h2 class="padding-10px">${json[i].name}</h2>
                                          <p class="text-margin-1rem text-green padding-10px">$50</p>
-                                         <div class="card-bg-img" style="background-image: url(${results[i].background_image});">
+                                         <div class="card-bg-img" style="background-image: url(${json[i].images[0].src});">
                                          </div>
                                          </a>
                                        </div>
